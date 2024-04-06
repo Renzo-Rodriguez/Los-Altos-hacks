a81599ccac13a1f64f93faaea918930c7678ec8a
LamSauced <lnguyen0606@outlook.com>
2024-04-06 14:58:12 -0700

    transparent overlay

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let mainWindow
 
-  win.loadFile('index.html')
-}
+// const createWindow = () => {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   win.loadFile('index.html')
+// }
 
 app.whenReady().then(() => {
-  createWindow()
+  mainWindow = new BrowserWindow({
+      width: 1920,
+      height: 1080,
+      transparent: true,
+      frame: false,
+      alwaysOnTop: true,
+    });
+  mainWindow.loadFile(`index.html`);
+  mainWindow.setIgnoreMouseEvents(true);
+  mainWindow.setFocusable(false);
 })
 
 app.on('window-all-closed', () => {
